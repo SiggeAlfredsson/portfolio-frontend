8bc68010c58b13b175ad9d6fb39befc949776d61
Sigge <saorust@hotmail.com>
2024-04-27 14:20:04 +0200

    view post updt

--- src/app/core/pages/view-post/view-post.component.ts
+++ src/app/core/pages/view-post/view-post.component.ts
@@ -19,6 +19,7 @@ export class ViewPostComponent implements OnInit {
   post: any;
   images: SafeUrl[] = [];
   newCommentText: string = "";
+  postId?: number;
 
   constructor(
     private route: ActivatedRoute,
@@ -31,28 +32,33 @@ export class ViewPostComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
-      // Use a non-null assertion operator or handle the null case explicitly
       const postIdStr = params.get('postId');
       if (postIdStr) {
-        const postId = +postIdStr; // Convert postIdStr to number using the unary plus
-        this.postService.getPostById(postId).subscribe(post => {
-          this.post = post;
-          if (post && post.picturesIds) {
-            console.log(post.picturesIds)
-            post.picturesIds.forEach(id => {
-              this.pictureService.getImageById(id).subscribe(blob => {
-                const objectURL = URL.createObjectURL(blob);
-                this.images.push(this.sanitizer.bypassSecurityTrustUrl(objectURL));
-              });
-            });
-          }
-        });
+        const postId = +postIdStr; 
+        this.postId = postId
+        this.loadPost();
       } else {
         console.error('Post ID is missing in the route parameters.');
-        // Optionally redirect the user or show an error message
       }
     });
   }
+
+  loadPost() {
+    if(this.postId) {
+    this.postService.getPostById(this.postId).subscribe(post => {
+      this.post = post;
+      if (post && post.picturesIds) {
+        console.log(post.picturesIds)
+        post.picturesIds.forEach(id => {
+          this.pictureService.getImageById(id).subscribe(blob => {
+            const objectURL = URL.createObjectURL(blob);
+            this.images.push(this.sanitizer.bypassSecurityTrustUrl(objectURL));
+          });
+        });
+      }
+    });
+  }
+}
 
   openImageDialog(imageSrc: any) {
     console.log(imageSrc)
@@ -64,21 +70,23 @@ export class ViewPostComponent implements OnInit {
 }
 
 toggleLike(post: Post): void {
-  // Implement like functionality
+  this.postService.likePost(post.id).subscribe(() => {
+    this.loadPost()
+  })
 }
 
 toggleStar(post: Post): void {
-  // Implement star functionality
-}
+  this.postService.starPost(post.id).subscribe(() => {
+    this.loadPost()
+  })  }
 submitComment(): void {
   const comment = {
-    // userId: this.userId, 
     postId: this.post.id,
     text: this.newCommentText
   };
-  // Here you should call an API to save the comment
+
   this.postService.addComment(comment.postId, comment.text).subscribe(() => {
-    
+    this.loadPost()
   })
 
   this.newCommentText = ''; // Clear the input after submission
